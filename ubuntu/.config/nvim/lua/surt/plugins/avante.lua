a3495a2d4e0a79a8c955187f00bfbda704f4fabb
SurturTawers <davtorresga@gmail.com>
2025-06-11 23:25:58 -0400

    DAP integrations, copilot in avante, aerial toggle key modified

--- ubuntu/.config/nvim/lua/surt/plugins/avante.lua
+++ ubuntu/.config/nvim/lua/surt/plugins/avante.lua
@@ -63,7 +63,7 @@ return {
 	"yetone/avante.nvim",
 	event = "VeryLazy",
 	version = false, -- Never set this value to "*"! Never!
-	opts = configs.ollama,
+	opts = configs.copilot,
 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
 	build = "make",
 	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
